f0bd7490b9c8bd2e99269164de5c4725493c72fe
ACECR <36438060+ACECR@users.noreply.github.com>
2018-11-17 18:00:14 +0330

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='parsivar',
 	  packages=['parsivar'],
-	  version='0.1.4',
+	  version='0.1.5',
 	  description='Python library for preprocessing Persian text.',
 	  author='ICT',
 	  author_email='',
