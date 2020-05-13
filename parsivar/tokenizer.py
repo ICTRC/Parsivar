5724e0bb03e5c5da1c0d80621662332f0d3ef0a2
ACECR <bero.1370@yahoo.com>
2020-05-13 19:13:41 +0430

    v0.2.3

--- parsivar/tokenizer.py
+++ parsivar/tokenizer.py
@@ -17,27 +17,35 @@ class Tokenizer():
         doc_string = re.sub(pattern, 'floatingpointnumber', doc_string)
 
         pattern = r'([!\.\?؟]+)[\n]*'
-        suse = re.findall(pattern, doc_string)
-        doc_string = re.sub(pattern, '\t\t', doc_string)
+        tmp = re.findall(pattern, doc_string)
+        doc_string = re.sub(pattern, self.add_tab, doc_string)
 
         pattern = r':\n'
-        suse = re.findall(pattern, doc_string)
-        doc_string = re.sub(pattern, '\t\t', doc_string)
+        tmp = re.findall(pattern, doc_string)
+        doc_string = re.sub(pattern, self.add_tab, doc_string)
 
         pattern = r';\n'
-        suse = re.findall(pattern, doc_string)
-        doc_string = re.sub(pattern, '\t\t', doc_string)
+        tmp = re.findall(pattern, doc_string)
+        doc_string = re.sub(pattern, self.add_tab, doc_string)
 
         pattern = r'؛\n'
-        suse = re.findall(pattern, doc_string)
-        doc_string = re.sub(pattern, '\t\t', doc_string)
+        tmp = re.findall(pattern, doc_string)
+        doc_string = re.sub(pattern, self.add_tab, doc_string)
 
         pattern = r'[\n]+'
-        doc_string = re.sub(pattern, '\t\t', doc_string)
+        doc_string = re.sub(pattern, self.add_tab, doc_string)
 
         for number in nums_list:
             pattern = 'floatingpointnumber'
             doc_string = re.sub(pattern, number, doc_string, 1)
 
         doc_string = doc_string.split('\t\t')
+        doc_string = [x for x in doc_string if len(x) > 0]
         return doc_string
+
+    def add_tab(self, mystring):
+        mystring = mystring.group()  # this method return the string matched by re
+        mystring = mystring.strip(' ')  # ommiting the whitespace around the pucntuation
+        mystring = mystring.strip('\n') # ommiting the newline around the pucntuation
+        mystring = " " + mystring + "\t\t"  # adding a space after and before punctuation
+        return mystring
